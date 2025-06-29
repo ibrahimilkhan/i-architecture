a5e46aa9cb7ac7f37515389de82c44720ffd7f96
ibrahimilkhan <ibrahimilkhan@hotmail.com>
2025-06-29 22:34:25 +0300

    Paginate class

--- src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
+++ src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq.Expressions;
+using Core.Persistence.Paging;
 using Microsoft.EntityFrameworkCore.Query;
 
 namespace Core.Persistence.Repositories;
@@ -13,7 +14,7 @@ public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity> where TE
         bool enableTracking = false,
         CancellationToken cancellationToken = default);
 
-    Task<IPaginate<TEntity>> GetListAsync(
+    Task<Paginate<TEntity>> GetListAsync(
         Expression<Func<TEntity, bool>>? predicate = null,
         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
@@ -23,7 +24,7 @@ public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity> where TE
         bool enableTracking = true,
         CancellationToken cancellationToken = default);
 
-    Task<IPaginate<TEntity>> GetListDynamicAsync(
+    Task<Paginate<TEntity>> GetListDynamicAsync(
         DynamicQuery dynamic,
         Expression<Func<TEntity, bool>>? predicate = null,
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
